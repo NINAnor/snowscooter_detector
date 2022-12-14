32000558cb6b6d4eb9f1af3f4919b2b30321977a
Benjamin Cretois <benjamin.cretois@nina.no>
2022-12-14 14:43:35 +0100

    Add computation of harmonic ratio

--- inference/predict.py
+++ inference/predict.py
@@ -7,14 +7,17 @@ import glob
 import yaml
 import csv
 import fs
+import tqdm
 
 from torch.quantization import quantize_dynamic
 from torch.utils.data import DataLoader
 from yaml.loader import FullLoader
 from multiprocessing import Pool
 from fs.sshfs import SSHFS
+from model.custom_model import CustomAudioCLIP
 
 from utils.utils_inference import AudioList
+from utils.audio_signal import AudioSignal
 
 def doConnection(connection_string):
 
@@ -58,25 +61,52 @@ def parseInputFiles(filesystem, input_path, workers, worker_idx, array_job=False
 
     return files
 
-def initModel(model_path):
-    model = CustomAudioCLIP(num_target_classes=2)
+def initModel(model_path, device):
+    model = CustomAudioCLIP(num_target_classes=2).to(device)
     model = model.load_from_checkpoint(model_path, num_target_classes=2)
     model.eval()
     #m_q = quantize_dynamic(m, qconfig_spec={torch.nn.Linear, torch.nn.Conv2d}, dtype=torch.qint8)
     return model
 
-def predict(testLoader, model, device):
+def compute_hr(array):
+
+    signal = AudioSignal(samples = array, fs=44100)
+
+    signal.apply_butterworth_filter(
+            order=18, Wn=np.asarray([1, 600]) / (signal.fs / 2)
+        )
+    signal_hr = signal.harmonic_ratio(
+            win_length=int(1 * signal.fs),
+            hop_length=int(0.1 * signal.fs),
+            window="hamming",
+        )
+    hr = np.mean(signal_hr)
+    
+    return hr
+
+def predict(testLoader, model, device, threshold=0.95):
 
     proba_list = []
+    hr_list = []
 
     for array in testLoader:
+
+        # Compute confidence for the DL model
         tensor = torch.tensor(array)
         tensor = tensor.to(device)
         output = model(tensor)
         output = np.exp(output.cpu().detach().numpy())
         proba_list.append(output[0])
 
-    return proba_list
+        # Compute HR if confidence is more than a threshold
+        max_value = output[0].max()
+        if max_value >= threshold:
+            hr = compute_hr(np.array(array))
+            hr_list.append(hr)
+        else:
+            hr_list.append(0)
+
+    return proba_list, hr_list
 
 def get_outname(input, outfolder):
 
@@ -103,24 +133,24 @@ def get_outname(input, outfolder):
 
     return outname
 
-def write_results(prob_array, outname):
+def write_results(prob_audioclip_array, hr_array, outname):
 
     # Store the array result in a CSV friendly format
     rows_for_csv = []
     idx_begin = 0
 
-    for item in prob_array:
+    for item_audioclip, item_hr in zip(prob_audioclip_array, hr_array):
+
         # Get the properties of the detection (start, end, label and confidence)
         idx_end = idx_begin + 3
-        arr = np.array(item)
-        label = np.argmax(arr, axis=0)
-        max_value = arr.max()
-
-        # Map the label index to its category
+        conf = np.array(item_audioclip)
+        label = np.argmax(conf, axis=0)
+        max_value = conf.max()
+        hr = np.array(item_hr)
 
         # If the label is not "soundscape" then write the row:
         if label != 0:
-            item_properties = [idx_begin, idx_end, label, max_value]
+            item_properties = [idx_begin, idx_end, label, max_value, hr]
             rows_for_csv.append(item_properties)
 
         # Update the start time of the detection
@@ -129,7 +159,7 @@ def write_results(prob_array, outname):
     with open(outname, 'w') as file:
 
         writer = csv.writer(file)
-        header = ["start_detection", "end_detection", "label", "confidence"]
+        header = ["start_detection", "end_detection", "label", "confidence", "hr"]
 
         writer.writerow(header)
         writer.writerows(rows_for_csv)
@@ -148,8 +178,8 @@ def analyzeFile(filesystem, file_path, model, out_folder, device, batch_size=1, 
         list_preds = AudioList().get_processed_list(filesystem, file_path)
         predLoader = DataLoader(list_preds, batch_size=batch_size, num_workers=num_workers, pin_memory=False)
 
-        pred_array = predict(predLoader, model, device)
-        write_results(pred_array, outname)
+        pred_audioclip_array, pred_hr_array  = predict(predLoader, model, device)
+        write_results(pred_audioclip_array, pred_hr_array, outname)
 
         # Give the tim it took to analyze file
         delta_time = (datetime.datetime.now() - start_time).total_seconds()
@@ -210,9 +240,9 @@ if __name__ == "__main__":
     print("Found {} files to analyze".format(len(flist)))
 
     # Analyze files
-    for entry in flist:
+    for entry in tqdm.tqdm(flist):
         print("Analysing {}".format(entry))
         try:
             analyzeFile(myfs, entry, model, cfg["OUTPUT_PATH"],  device=cfg["DEVICE"], batch_size=1, num_workers=1)
         except:
-            print("File {} failed to be analyzed".format(entry))
+            #print("File {} failed to be analyzed".format(entry))
